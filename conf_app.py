9e74a07e8a13d2632b63c60c107195f229d1b07a
YashSunidhi <smnitrkl50@gmail.com>
2023-10-16 13:17:25 +0200

    Update conf_app.py

--- conf_app.py
+++ conf_app.py
@@ -26,7 +26,7 @@ uploaded_file1 = st.sidebar.file_uploader("Upload a Concept csv File",type= 'csv
 if uploaded_file is not None:
     if uploaded_file1 is not None:
         # if uploaded_file2 is not None:
-        def main_page(uploaded_file = uploaded_file,uploaded_file1 = uploaded_file1,uploaded_file2 = uploaded_file2):
+        def main_page(uploaded_file = uploaded_file,uploaded_file1 = uploaded_file1):
             #create_space(1)
             with st.expander(label="# To Compare different Summarization and Provide feedback on what good will look like", expanded=False):
                 st.markdown("""
