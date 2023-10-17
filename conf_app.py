9e01d57f5571026edad73748a4d96270be4216d7
YashSunidhi <smnitrkl50@gmail.com>
2023-10-17 21:05:43 +0200

    Update conf_app.py

--- conf_app.py
+++ conf_app.py
@@ -154,7 +154,7 @@ def concept_view_1():
         st.markdown("<h4 style='text-align: center; color: black;'> Some Interesting Facts about Ectrims 2023 </h4>", unsafe_allow_html=True)
         colh, coli, colj, colk, coll, colf, colg = st.columns(7)
         author = df[['Author','Twitter Followers']].drop_duplicates().sort_values('Twitter Followers',ascending=False)['Author'][:5]
-        colh.write(label="Author with Highest Number of Followers",author)
+        colh.write(author)
 
         
     
