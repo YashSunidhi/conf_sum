9514b32b023f04f4c4bee7cc829b49822a9bf573
YashSunidhi <smnitrkl50@gmail.com>
2023-10-17 21:02:45 +0200

    Update conf_app.py

--- conf_app.py
+++ conf_app.py
@@ -149,9 +149,13 @@ def concept_view_1():
         colf.metric(label="Number of Countries", value=df['Country Code'].nunique())
         colg.metric(label="Number of Categories", value=df['Category Details'].nunique())
         #colc.metric(label="Number of Font Size Used", value=tt.size*1000.nunique())
-        st.markdown("<h4 style='text-align: center; color: black;'> Top Story from Ectrims 2023  </h4>", unsafe_allow_html=True)
-        st.write(dc['Summary'][0])
-        #col2.write(dc['tag'][0])
+
+        #col1, col2 = st.columns([1,1])
+        st.markdown("<h4 style='text-align: center; color: black;'> Some Interesting Facts about Ectrims 2023 </h4>", unsafe_allow_html=True)
+        colh, coli, colj, colk, coll, colf, colg = st.columns(7)
+        author = df[['Author','Twitter Followers']].drop_dupicates().sort_values('Twitter Followers',ascending=False)['Author'][:5]
+        colh.text_box("Author with Highest Number of Followers",author)
+
         
     
     with tab5:
